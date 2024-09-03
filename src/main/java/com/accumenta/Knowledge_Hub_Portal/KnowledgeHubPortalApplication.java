524f24702744adc64afb9da0823dc4030f0ddcd6
Venkatrao-dev <medavenkatrao@accumenta.co.in>
2024-09-03 11:09:20 +0530

    first commit

---
+++ src/main/java/com/accumenta/Knowledge_Hub_Portal/KnowledgeHubPortalApplication.java
@@ -0,0 +1,13 @@
+package com.accumenta.Knowledge_Hub_Portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeHubPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowledgeHubPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accumenta/Knowledge_Hub_Portal/KnowledgeHubPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accumenta.Knowledge_Hub_Portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeHubPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
